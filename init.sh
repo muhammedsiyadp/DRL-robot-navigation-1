ab70049f8dff5cc368e819a857e9218be9977a0d
muhammedsiyadp <muhammedsiyadofficial@gmail.com>
2025-02-23 18:41:19 +0530

    added gitignore and init script

---
+++ init.sh
@@ -0,0 +1,8 @@
+export ROS_HOSTNAME=localhost
+export ROS_MASTER_URI=http://localhost:11311
+export ROS_PORT_SIM=11311
+export GAZEBO_RESOURCE_PATH=~/DRL-robot-navigation/catkin_ws/src/multi_robot_scenario/launch
+source ../.bashrc
+cd catkin_ws
+source devel_isolated/setup.bash
+cd ..
