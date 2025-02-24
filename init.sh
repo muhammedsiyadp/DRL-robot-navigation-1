e5b2b27d64b45a9aa4775de407bcd0218ff5bd20
muhammedsiyadp <muhammedsiyadofficial@gmail.com>
2025-02-24 16:35:20 +0530

    improved init sketch

--- init.sh
+++ init.sh
@@ -1,8 +1,12 @@
+#!/bin/bash
+
 export ROS_HOSTNAME=localhost
 export ROS_MASTER_URI=http://localhost:11311
 export ROS_PORT_SIM=11311
 export GAZEBO_RESOURCE_PATH=~/DRL-robot-navigation/catkin_ws/src/multi_robot_scenario/launch
 source ../.bashrc
-cd catkin_ws
+cd catkin_world_ws
+source devel/setup.bash
+cd ../catkin_ws
 source devel_isolated/setup.bash
-cd ..
+echo "done"
